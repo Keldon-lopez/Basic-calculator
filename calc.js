2800cdcfe9b8f181723af34c9c970b7d574f754f
Keldon Lopez <keldon.lopz@gmail.com>
2023-01-14 14:34:38 -0700

    Added square root and changed error message

--- calc.js
+++ calc.js
@@ -1,39 +1,39 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+reader.question("What would you like to calculate?", function (input) {
+  const tokens = input.split(" ");
 
+//   console.log(tokens);
 
-reader.question("What would you like to calculate?", function(input){
-	const tokens = input.split(' ');
+  const mathSymbol = tokens[0];
+  const num1 = Number(tokens[1]);
+  const num2 = Number(tokens[2]);
 
-	console.log(tokens);
-	
-	const mathSymbol = tokens[0];
-	const num1 = Number(tokens[1]);
-	const num2 = Number(tokens[2]);
+//   console.log("mathSymbol", mathSymbol);
+//   console.log("num1", num1);
+//   console.log("num2", num2);
 
-	console.log('mathSymbol', mathSymbol);
-	console.log('num1', num1);
-	console.log('num2', num2);
+ if (mathSymbol === "sqrt") {
+	console.log(`Square root of ${num1} is`, Math.sqrt(num1));
+  } else if (mathSymbol === "+") {
+    console.log(num1 + num2);
+  } else if (mathSymbol === "-") {
+    console.log(num1 - num2);
+  } else if (mathSymbol === "*") {
+    console.log(num1 * num2);
+  } else if (mathSymbol === "/") {
+    console.log(num1 / num2);
+  } else {
+    console.log(
+      "syntax error. Please format input as 'symbol number number' or for square root enter as 'sqrt number' "
+    );
+  }
 
-	if (mathSymbol === "+" ){
-		console.log(num1+num2)
-	}else if( mathSymbol === "-"	){
-		console.log(num1-num2)
-	} else if( mathSymbol === "*"	){
-		console.log(num1*num2)
-	} else if (mathSymbol === "/"){
-		console.log(num1/num2)
-	} else{
-		console.log("Math symbol not provided. Please format input as symbol number number ")
-	}
-
-
-	// This line closes the connection to the command line interface.
-	reader.close()
-
+  // This line closes the connection to the command line interface.
+  reader.close();
 });
